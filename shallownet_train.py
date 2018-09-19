af226845e2eb011d6390bb61cabae5592e7cdad6
Andreas Gounaris <andreas.gounaris@gmail.com>
2018-09-19 14:00:09 +0300

    remove invertbinarize from this version

--- shallownet_train.py
+++ shallownet_train.py
@@ -1,15 +1,18 @@
 # USAGE
-# python shallownet_room.py --dataset ../pistreamer/pics/basement --model shallownet_room.hdf5
+# python shallownet_train.py --dataset data/basement_3 --model cascades/shallownet_basement_3.hdf5
 
 # import the necessary packages
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.metrics import hamming_loss
 from utils.preprocessing import ImageToArrayPreprocessor
 from utils.preprocessing import SimplePreprocessor
 from utils.datasets import SimpleDatasetLoader
 from utils.nn.conv import ShallowNet
+from utils.nn.conv import ShallowNetBinary
 from keras.optimizers import SGD
+from keras.utils import plot_model
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,11 +27,15 @@ ap.add_argument("-m", "--model", required=True,
                 help="path to output model")
 args = vars(ap.parse_args())
 
-epochs = 100
+dataset = args["dataset"]
+model_name = args["model"]
+
+epochs = 10
+batch_size = 32
 
 # grab the list of images that we'll be describing
 print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
+imagePaths = list(paths.list_images(dataset))
 
 # Open a single image to acquire dimensions
 image = cv2.imread(imagePaths[0])
@@ -60,27 +67,36 @@ testY = LabelBinarizer().fit_transform(testY)
 print("[INFO] compiling model...")
 opt = SGD(lr=0.005)
 model = ShallowNet.build(width=iW, height=iH, depth=3, classes=len(classes))
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-              metrics=["accuracy"])
-# model.compile(loss="binary_crossentropy", optimizer="rmsprop",
-#               metrics=["accuracy"])
+
+# model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+# model.compile(loss="binary_crossentropy", optimizer="adadelta", metrics=["accuracy"])
+# model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["acc"])
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-              batch_size=8, epochs=epochs, verbose=1)
+              batch_size=batch_size, epochs=epochs, verbose=1)
 
 # save the network to disk
 print("[INFO] serializing network...")
-model.save(args["model"])
+model.save(model_name)
+# Save model graph to disk
+model_graph = "{}.png".format(model_name)
+plot_model(model, to_file=model_graph, show_shapes=True)
 
 # evaluate the network
 print("[INFO] evaluating network...")
-predictions = model.predict(testX, batch_size=8)
+predictions = model.predict(testX, batch_size=batch_size)
+
+threshold = 0.5
+hamm_loss = hamming_loss(testY.argmax(axis=1), predictions.argmax(axis=1) > threshold)
+print("Hamming Loss: {}".format(hamm_loss))
+
+# print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1)))
 print(classification_report(testY.argmax(axis=1),
-                            predictions.argmax(axis=1),
+                            predictions.argmax(axis=1) > threshold,
                             target_names=classes))
-# target_names=["cat", "dog", "panda"]))
 
 # plot the training loss and accuracy
 plt.style.use("ggplot")
@@ -93,4 +109,5 @@ plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
+plt.savefig("{}.train.png".format(model_name))
 plt.show()
--- utils/preprocessing/__init__.py
+++ utils/preprocessing/__init__.py
@@ -1,4 +1,3 @@
 # import the necessary packages
 from .imagetoarraypreprocessor import ImageToArrayPreprocessor
 from .simplepreprocessor import SimplePreprocessor
-from .invertbinarize import InvertBinarize
